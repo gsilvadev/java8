e1337ade32a27231282df1a99d0cc99570954728
Gilson Oliveira <gsilva.dev@gmail.com>
2017-04-07 18:01:24 -0300

    Commit inicial

---
+++ src/java8/OrdenaStrings.java
@@ -0,0 +1,25 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+	
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<>();
+		palavras.add("Casa do Código");
+		palavras.add("Caelum");
+		palavras.add("Alura ensino e inovação");
+
+		palavras.sort(Comparator.comparing(String::length));
+
+		palavras.replaceAll(str -> str.replace("a", "t"));
+
+		palavras.removeIf(t -> t.contains("l"));
+
+		palavras.forEach(System.out::println);
+		
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
+	}
+}
