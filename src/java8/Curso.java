87fc02d8de6dd67881937cb28a11b520c7d7d08f
Gilson Oliveira <gsilva.dev@gmail.com>
2017-04-10 18:12:35 -0300

    Continuação Streams.

---
+++ src/java8/Curso.java
@@ -0,0 +1,38 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Curso {
+	private String nome;
+	private int alunos;
+
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+}
+
+class ExemploCursos {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.stream()
+			.filter(c -> c.getAlunos() > 50)
+			.map(c -> c.getNome())
+			.forEach(System.out::println);
+	}
+}
--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -12,14 +12,14 @@ public class OrdenaStrings {
 		palavras.add("Caelum");
 		palavras.add("Alura ensino e inovação");
 
-		palavras.sort(Comparator.comparing(String::length));
+		palavras.sort(String.CASE_INSENSITIVE_ORDER);
 
-		palavras.replaceAll(str -> str.replace("a", "t"));
+//		palavras.replaceAll(str -> str.replace("a", "t"));
 
-		palavras.removeIf(t -> t.contains("l"));
+//		palavras.removeIf(t -> t.contains("l"));
 
 		palavras.forEach(System.out::println);
 		
-		new Thread(() -> System.out.println("Executando um Runnable")).start();
+//		new Thread(() -> System.out.println("Executando um Runnable")).start();
 	}
 }
